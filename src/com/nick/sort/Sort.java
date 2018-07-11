f40f8dc4d3f96815925a8e3becc054eb0cc2c5a1
小强 <649197313@qq.com>
2018-07-11 15:40:35 +0800

    Introduction to Algorithms & quicksort

--- src/com/nick/sort/Sort.java
+++ src/com/nick/sort/Sort.java
@@ -6,11 +6,9 @@ import java.util.Random;
 public class Sort {
 
     public static void main(String[] args) {
-//        int[] arr = {6, 1, 8, 12, 72, 5, 36};
-        int[] arr = {10, 80, 24, 52, 49, 22, 57, 9, 89, 50, 67, 21, 38, 77, 44, 49, 70, 33, 95, 97, 58, 57, 64, 49, 78, 30, 60, 86, 58, 86};
-//        int[] arr = {10, 80, 24, 52, 49, 22, 57, 9, 50, 21};
-//        int[] arr = {10, 9, 21};
-        quickSort(arr, 0, arr.length - 1);
+//        int[] arr = {3, 8, 1, 4, 2};
+            int[] arr = {10, 80, 24, 52, 49, 22, 57, 9, 89, 50, 67, 21, 38, 77, 44, 49, 70, 33, 95, 97, 58, 57, 64, 49, 78, 30, 60, 86, 58, 86};
+        quickSort2(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -23,6 +21,32 @@ public class Sort {
             }
             arr[j] = tmp;
         }
+    }
+
+    static void quickSort2(int[] arr, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+        int k = partition(arr, low, high);
+        quickSort2(arr, low, k - 1);
+        quickSort2(arr, k + 1, high);
+    }
+    static int partition(int[] arr, int low, int high) {
+        int pivot = arr[low];
+        int cursor = low + 1;
+        int k = low;
+        while (cursor <= high) {
+            if (arr[cursor] <pivot){
+                k++;
+                int tmp = arr[k];
+                arr[k] = arr[cursor];
+                arr[cursor] = tmp;
+            }
+            cursor++;
+        }
+        arr[low] = arr[k];
+        arr[k] = pivot;
+        return k;
     }
 
 
